78311a5ee0713beb34888ca0b0deee700fdd1ae5
Anant Dabhi <anant.dabhi@tatvasoft.com>
2024-11-29 14:17:06 +0530

    First version release

---
+++ Additions/NearPay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Java.Interop.JniEnvironment;
+
+namespace IO.Nearpay.Sdk
+{
+    public partial class NearPay
+    {
+        ~NearPay()
+        {
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MauiAndroid-nearpaySDK
 Android MAUI Xamarin library for Nearpay The Tap to Pay on Phone SDK
+
+I have bind : 
+
+dependencies {
+    implementation "io.nearpay:nearpay-sdk-store:2.1.88"
+}
+
